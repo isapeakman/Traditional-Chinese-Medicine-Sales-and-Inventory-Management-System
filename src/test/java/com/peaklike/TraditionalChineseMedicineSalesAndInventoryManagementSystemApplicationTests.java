9172619498b98b18c529cebc3d8445a2f38af8a6
peak-like <2095991530@qq.com>
2024-06-28 17:19:13 +0800

    init project

---
+++ src/test/java/com/peaklike/TraditionalChineseMedicineSalesAndInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaklike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraditionalChineseMedicineSalesAndInventoryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
